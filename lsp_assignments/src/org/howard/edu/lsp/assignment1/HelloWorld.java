1af9f9385d38d6dbc5c23eb8062458470abe8574
Pradeep Lamichhane <lcpradip2000@gmail.com>
2022-10-17 23:39:14 -0400

    commit for assignment1 Signed-off-by: Pradeep Lamichhane <lcpradip2000@gmail.com>

---
+++ lsp_assignments/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,11 @@
+package org.howard.edu.lsp.assignment1;
+/**
+ * 
+ * @author pradeep
+ *
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
